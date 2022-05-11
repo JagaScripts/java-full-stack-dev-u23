69773aa1a009eb53894156c606d1a7be72a8ee8d
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-11 15:25:03 +0200

    Archivos y projectos importados

---
+++ gradle/src/test/java/com/josea/u23/gradle/GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josea.u23.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
